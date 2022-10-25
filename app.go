e056deecc1bce6dc76af90f73f707986bd661070
= <=>
2022-10-25 18:14:12 +0200

    more logging

--- app.go
+++ app.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/juju/fslock"
@@ -20,6 +21,7 @@ func (t *App) HasResource(path string) bool {
 
 func (t *App) SetResource(path string, data interface{}) error {
 	t.resources[path] = data
+	log.Printf("app: resource [%s] updated\n", path)
 	return nil
 }
 
@@ -27,11 +29,13 @@ func (t *App) GetResource(path string) (interface{}, error) {
 	if !t.HasResource(path) {
 		return nil, fmt.Errorf("no such resource: [%s]", path)
 	}
+	log.Printf("app: resource [%s] queried\n", path)
 	return t.resources[path], nil
 }
 
 func (t *App) RemoveResource(path string) {
 	delete(t.resources, path)
+	log.Printf("app: resource [%s] removed\n", path)
 }
 
 func (t *App) Persist() error {
@@ -50,6 +54,7 @@ func (t *App) Persist() error {
 	if err = json.NewEncoder(file).Encode(t.resources); err != nil {
 		return err
 	}
+	log.Printf("app: state persisted to %s\n", path)
 	return nil
 }
 
@@ -68,6 +73,7 @@ func (t *App) Restore() error {
 	if json.NewDecoder(file).Decode(&t.resources); err != nil {
 		return err
 	}
+	log.Printf("app: state restored from %s\n", path)
 	return nil
 }
 
